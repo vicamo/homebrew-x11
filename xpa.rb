dd3fa5997bad2ee2fe4a66eff193164d5232fa83
Baptiste Fontaine <batifon@yahoo.fr>
2015-11-25 12:16:32 +0100

    xpa 2.1.17
    Closes #160.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- xpa.rb
+++ xpa.rb
@@ -1,7 +1,8 @@
 class Xpa < Formula
+  desc "Seamless communication between Unix programs"
   homepage "http://hea-www.harvard.edu/RD/xpa/"
-  url "http://hea-www.harvard.edu/saord/download/xpa/xpa-2.1.15.tar.gz"
-  sha256 "ac0e041f9115757fbcbfeb377cb5833544815a70f2b46f6edfbf6d1239ae690a"
+  url "https://github.com/ericmandel/xpa/archive/v2.1.17.tar.gz"
+  sha256 "c95ae1d6c5353226a90f29007822e98da42a036af2f7326a21d2f66189c49751"
 
   depends_on :x11
 
